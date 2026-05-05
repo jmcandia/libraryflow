174ef5f975f93f4dd8386de5abed64eb555647c4
José Miguel Candia <jmcandia@gmail.com>
2026-05-05 14:12:17 -0400

    docs: add JavaDocs to the code:
    - authors-service: AuthorsServiceApplication
    - users-service: UsersServiceApplication
    
    AFFECTED: authors-service, users-service

--- users-service/src/main/java/io/libraryflow/users/UsersServiceApplication.java
+++ users-service/src/main/java/io/libraryflow/users/UsersServiceApplication.java
@@ -3,9 +3,28 @@ package io.libraryflow.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal de la aplicación Spring Boot para el servicio de usuarios.
+ * Esta clase se encarga de iniciar la aplicación y configurar el contexto de
+ * Spring.
+ */
 @SpringBootApplication
 public class UsersServiceApplication {
 
+	/**
+	 * Método principal que inicia la aplicación Spring Boot. Este método es el
+	 * punto de entrada de la aplicación y se encarga de lanzar el contexto de
+	 * Spring, lo que permite que se configuren y se inicien todos los componentes
+	 * necesarios para el funcionamiento del servicio de usuarios.
+	 * 
+	 * @param args String[] - Argumentos de línea de comandos que se pueden pasar al
+	 *             iniciar la aplicación. Estos argumentos pueden ser utilizados
+	 *             para configurar aspectos específicos de la aplicación, como el
+	 *             puerto del servidor, el perfil de Spring activo, entre otros. Sin
+	 *             embargo, en la mayoría de los casos, no se requieren argumentos
+	 *             para iniciar la aplicación, y se pueden omitir al ejecutar el
+	 *             comando de inicio.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(UsersServiceApplication.class, args);
 	}
